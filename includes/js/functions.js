6611c28a9da92023687e6b27be5b6b9a4ff72f61
JMarqz <jmarqz04@gmail.com>
2014-07-02 20:02:15 -0500

    Matinal App
    AppAvailability v0.3.0

--- includes/js/functions.js
+++ includes/js/functions.js
@@ -1,64 +1,50 @@
-//MENU
+// CONFIGURACIÓN MENU
 $(function() {
     $('#menu').mmenu({
         position: "left"
     });
 });
 
-//Función para abrir el menú deslizando el dedo
+// ABRIR MENU DESLIZANDO DERECHA
 $(document).on('pageinit',function(){
     $("#page").on("swiperight",function(){
         $("#menu").trigger( "open.mm" );
     });
 });
 
-
-//ACTUALIZAR
+// ACTUALIZAR
 function actualizar(){
     $('#cargando').show();
     setTimeout('location.reload(true);',0);
 }
 
-/* PROBANDO LAS APLICACIONES INSTALADAS */
+// ABRIR TWITTER NATIVO 
 function abrirTwitter(userName){
     document.addEventListener("deviceready", onDeviceReady, false);
     function onDeviceReady() {
         var plataforma = device.platform;
         var twitter;
 
-        //Saber si es Android o iOS
         if (plataforma == "Android") {
             twitter = "com.twitter.android";
         } else if(plataforma == "iOS"){
             twitter = "twitter://";
         };
 
-        //Checar Twitter v0.3.0
+        // AppAvailability v0.3.0
         appAvailability.check(
         twitter, // URI Scheme
         function() {  // Success callback
-            alert('Twitter está instalado');
             window.open("twitter://user?screen_name="+userName);
         },
         function() {  // Error callback
-            alert('Twitter NO está instalado');
             window.open("http://www.twitter.com/"+userName, "_system");
         });
-
-        //Checar Twitter v0.2.1
-        /*appAvailability.check(twitter, function(availability) {
-            // availability is either true or false
-            if(availability) {
-                window.open("twitter://user?screen_name="+userName);
-            } else{
-                window.open("http://www.twitter.com/"+userName, "_system");
-            }
-        });*/
-    }
+    }   
 }
 
-
-function abrirFB(user){
+// ABRIR FACEBOOK
+function abrirFB(userName){
     document.addEventListener("deviceready", onDeviceReady, false);
     function onDeviceReady(){
         var plataforma = device.platform;
@@ -70,12 +56,14 @@ function abrirFB(user){
             fb = "fb://";
         };
 
-        appAvailability.check(fb, function(availability) {
-            if(availability) {
-                window.open("fb://profile/"+user);
-            } else{
-                window.open("http://www.facebook.com/"+user, "_system");
-            }
+        // AppAvailability v0.3.0
+        appAvailability.check(
+        fb, // URI Scheme
+        function() {  // Success callback
+            window.open("fb://profile/"+userName);
+        },
+        function() {  // Error callback
+            window.open("http://www.facebook.com/"+userName, "_system");
         });
     }
 }
